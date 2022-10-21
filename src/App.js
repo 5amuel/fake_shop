46bf0ffe63b432c39201d3f3954f0bceb1b467bc
Samuel Martinez <samuel.martinez.morales@gmail.com>
2022-10-21 14:01:26 -0500

    Creation of components and setup of redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+
+import Header from './containers/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ProductListing from './containers/ProductListing'; 
 import './App.css';
+import ProductDetail from './containers/ProductDetail';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" component={ProductDetail} />
+          <Route>404 Not Found!</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
