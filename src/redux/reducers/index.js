c203674aba70c6342b3c33ebead0849ef2e59ba8
Samuel Martinez <samuel.martinez.morales@gmail.com>
2022-10-21 14:41:43 -0500

    finishing the productDetails Component

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,9 @@
 import {combineReducers} from 'redux';
-import { productReducer } from './productReducer';
+import { productReducer, selectedProductReducer } from './productReducer';
 
 const reducer = combineReducers ({
-    allProducts: productReducer
+    allProducts: productReducer,
+    product: selectedProductReducer
 })
 
 export default reducer
